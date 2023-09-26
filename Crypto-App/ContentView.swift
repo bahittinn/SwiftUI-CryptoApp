028eba36fed16b700812746dfe887971ecf4d473
bahittinn <bahittin84@gmail.com>
2023-09-26 21:22:48 +0300

    project setup

---
+++ Crypto-App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Crypto-App
+//
+//  Created by Bahittin on 26.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "bell")
+            Text("Hello").bold()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
